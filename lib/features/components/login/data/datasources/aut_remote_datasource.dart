c5e41fdb23e99ee9999cca1db0cf211133bc317e
DESKTOP-TTH3VV0\INTEL <daviddimas80@gmail.com>
2025-07-18 10:09:24 +0700

    add post

--- lib/features/components/login/data/datasources/aut_remote_datasource.dart
+++ lib/features/components/login/data/datasources/aut_remote_datasource.dart
@@ -5,6 +5,26 @@ import '../models/user_model.dart';
 class AuthRemoteDataSource {
   Future<UserModel> login(String email, String password) async {
     // Dummy API simulation
+    //   final url = Uri.parse('https://api.example.com/products');
+
+    // final response = await http.post(
+    //   url,
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //     'Accept': 'application/json',
+    //   },
+    //   body: jsonEncode({
+    //     'name': 'Produk Baru',
+    //     'price': 12000,
+    //   }),
+    // );
+
+    // if (response.statusCode == 200 || response.statusCode == 201) {
+    //   final data = jsonDecode(response.body);
+    //   print('Berhasil: $data');
+    // } else {
+    //   print('Gagal: ${response.statusCode}');
+    // }
     await Future.delayed(Duration(seconds: 1)); // simulasi delay
     final response = {"id": "1", "name": "David Patty"};
     return UserModel.fromJson(response);
--- lib/features/components/signup/data/datasources/aut_remote_datasource.dart
+++ lib/features/components/signup/data/datasources/aut_remote_datasource.dart
@@ -4,6 +4,31 @@ import '../models/signup.dart';
 
 class AuthRemoteDatasource {
   Future<SignupModel> signup(String email, String password) async {
+    //    final response = await http.get(
+    //   Uri.parse('https://api.example.com/products'),
+    // );
+
+    // final List<dynamic> data = jsonDecode(response.body);
+    //   final url = Uri.parse('https://api.example.com/products');
+
+    // final response = await http.post(
+    //   url,
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //     'Accept': 'application/json',
+    //   },
+    //   body: jsonEncode({
+    //     'name': 'Produk Baru',
+    //     'price': 12000,
+    //   }),
+    // );
+
+    // if (response.statusCode == 200 || response.statusCode == 201) {
+    //   final data = jsonDecode(response.body);
+    //   print('Berhasil: $data');
+    // } else {
+    //   print('Gagal: ${response.statusCode}');
+    // }
     await Future.delayed(Duration(seconds: 1));
     final response = {"status": "Ok", "responseMessages": "Success"};
     return SignupModel.fromJson(response);
